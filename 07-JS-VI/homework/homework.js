4d668c6d66c78d570a5f33ec263a4764eee54330
FernandaBustamante <fernandart.987@gmail.com>
2022-07-29 12:34:15 -0500

    Primer trabajo de CSS / HTML

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -18,7 +18,6 @@ function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
-
   return cb(n1, n2);
 }
 
@@ -28,7 +27,6 @@ function sumarArray(numeros, cb) {
   // No es necesario devolver nada
   //Tu código:
 
-  var numeros = [1, 2, 3, 4, 5]
   var suma = 0
   for (var i = 0; i<numeros.length; i++){
     suma = suma + numeros[i];
@@ -41,8 +39,10 @@ function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
-  for (var i = 0; i<array.length; i++)
-  cb(array[i])
+  for (var i = 0; i<array.length; i++){
+
+    cb(array[i])
+  }
 
 }
 
